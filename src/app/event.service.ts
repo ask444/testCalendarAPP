c2316f12e450625c414751b6bdbeb855ea1f930d
ashok <ashok.ng786@gmail.com>
2019-01-04 00:04:20 +0530

    updated the rupee symbol

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -2,7 +2,22 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import 'rxjs';
 import { of } from 'rxjs';
-
+var currency_symbols = {
+    'USD': '$', // US Dollar
+    'EUR': '€', // Euro
+    'GBP': '£', // British Pound Sterling
+    'ILS': '₪', // Israeli New Sheqel
+    'INR': '₹', // Indian Rupee
+    'JPY': '¥', // Japanese Yen
+    'KRW': '₩', // South Korean Won
+    'NGN': '₦', // Nigerian Naira
+    'PHP': '₱', // Philippine Peso
+    'PLN': 'zł', // Polish Zloty
+    'PYG': '₲', // Paraguayan Guarani
+    'THB': '฿', // Thai Baht
+    'UAH': '₴', // Ukrainian Hryvnia
+    'VND': '₫', // Vietnamese Dong
+};
 @Injectable()
 export class EventSesrvice {
     public getEvents(): Observable<any> {
@@ -10,27 +25,27 @@ export class EventSesrvice {
         const yearMonth = dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
         let data: any = [{
             id: 1,
-            title: 'Rs.2000',
+            title: currency_symbols.INR + '2000',
             start: yearMonth,
             allDay: true
         },
         {
             id: 2,
-            title: '$3992',
+            title: currency_symbols.INR+'3992',
             start: yearMonth + '-07',
             allDay: true
             // end: yearMonth + '-10'
         },
         {
             id: 3,
-            title: '$3995',
+            title: currency_symbols.INR+'3995',
             start: yearMonth + '-08',
             allDay: true
             // end: yearMonth + '-10'
         },
         {
             id: 4,
-            title: '$3990',
+            title: currency_symbols.INR+'3990',
             start: yearMonth + '-09',
             allDay: true
             // end: yearMonth + '-10'
@@ -46,7 +61,7 @@ export class EventSesrvice {
         //     start: yearMonth + '-16T16:00:00'
         // },
         {
-            title: '$2500',
+            title: currency_symbols.INR+'2500',
             start: yearMonth + '-11',
             // end: yearMonth + '-13',
             allDay: true
@@ -77,7 +92,7 @@ export class EventSesrvice {
         //     start: yearMonth + '-13T07:00:00'
         // },
         {
-            title: '$3299',
+            title: currency_symbols.INR+'3299',
             url: 'http://google.com/',
             start: yearMonth + '-28',
             allDay: true
