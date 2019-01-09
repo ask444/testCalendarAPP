60b1c8e7e6db275e2ca2783af4c80fdc7076a86a
ashok <ashok.ng786@gmail.com>
2019-01-10 01:08:48 +0530

    updated the calendar code.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,32 +31,167 @@ export class HomeComponent {
 
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
   language: string = "en";
+  calendarDate: any;
+  public calevents = [];
   constructor(protected eventService: EventSesrvice) { }
 
   ngOnInit() {
+   function gmod(n, m) {
+      return ((n % m) + m) % m;
+    }
+  
+   function kuwaiticalendar(adjust) {
+      var today = new Date(adjust);
+      // if (adjust) {
+      //   var adjustmili = 1000 * 60 * 60 * 24 * adjust;
+      //   var todaymili = today.getTime() + adjustmili;
+      //   var today = new Date(todaymili);
+      // }
+      var day = today.getDate();
+      var month = today.getMonth();
+      var year = today.getFullYear();
+      var m = month + 1;
+      var y = year;
+      if (m < 3) {
+        y -= 1;
+        m += 12;
+      }
+  
+      var a = Math.floor(y / 100.);
+      var b = 2 - a + Math.floor(a / 4.);
+      if (y < 1583) b = 0;
+      if (y == 1582) {
+        if (m > 10) b = -10;
+        if (m == 10) {
+          b = 0;
+          if (day > 4) b = -10;
+        }
+      }
+  
+      var jd = Math.floor(365.25 * (y + 4716)) + Math.floor(30.6001 * (m + 1)) + day + b - 1524;
+  
+      b = 0;
+      if (jd > 2299160) {
+        a = Math.floor((jd - 1867216.25) / 36524.25);
+        b = 1 + a - Math.floor(a / 4.);
+      }
+      var bb = jd + b + 1524;
+      var cc = Math.floor((bb - 122.1) / 365.25);
+      var dd = Math.floor(365.25 * cc);
+      var ee = Math.floor((bb - dd) / 30.6001);
+      day = (bb - dd) - Math.floor(30.6001 * ee);
+      month = ee - 1;
+      if (ee > 13) {
+        cc += 1;
+        month = ee - 13;
+      }
+      year = cc - 4716;
+  
+      if (adjust) {
+        var wd = gmod(jd + 1 - adjust, 7) + 1;
+      } else {
+        var wd = gmod(jd + 1, 7) + 1;
+      }
+  
+      var iyear = 10631. / 30.;
+      var epochastro = 1948084;
+      var epochcivil = 1948085;
+  
+      var shift1 = 8.01 / 60.;
+  
+      var z = jd - epochastro;
+      var cyc = Math.floor(z / 10631.);
+      z = z - 10631 * cyc;
+      var j = Math.floor((z - shift1) / iyear);
+      var iy = 30 * cyc + j;
+      z = z - Math.floor(j * iyear + shift1);
+      var im = Math.floor((z + 28.5001) / 29.5);
+      if (im == 13) im = 12;
+      var id = z - Math.floor(29.5001 * im - 29);
+  
+      var myRes = new Array(8);
+  
+      myRes[0] = day; //calculated day (CE)
+      myRes[1] = month - 1; //calculated month (CE)
+      myRes[2] = year; //calculated year (CE)
+      myRes[3] = jd - 1; //julian day number
+      myRes[4] = wd - 1; //weekday number
+      myRes[5] = id; //islamic date
+      myRes[6] = im - 1; //islamic month
+      myRes[7] = iy; //islamic year
+  
+      return myRes;
+    }
+  function  writeIslamicDate(adjustment) {
+      var wdNames = new Array("Ahad", "Ithnin", "Thulatha", "Arbaa", "Khams", "Jumuah", "Sabt");
+      var iMonthNames = new Array("Muharram", "Safar", "Rabi'ul Awwal", "Rabi'ul Akhir",
+        "Jumadal Ula", "Jumadal Akhira", "Rajab", "Sha'ban",
+        "Ramadan", "Shawwal", "Dhul Qa'ada", "Dhul Hijja");
+      var iDate = kuwaiticalendar(adjustment);
+      var outputIslamicDate = wdNames[iDate[4]] + ", "
+        + iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
+      return outputIslamicDate;
+    }
 
 
+    // $(function () {
+    //   $('#datetimepicker12').datetimepicker({
+    //     locale: 'ar',
+    //     inline: true,
+    //     sideBySide: true,
+    //     format: 'L'
+    //   });
+    //   $('#datetimepicker13').datetimepicker({
+    //     locale: 'en',
+    //     inline: true,
+    //     sideBySide: true,
+    //     format: 'L'
+    //   });
+    // });
 
-    $(function() {
-      $('#datetimepicker12').datetimepicker({
-        locale: 'ar',
-        inline: true,
-        sideBySide: true,
-        format: 'L'
-      });
-      $('#datetimepicker13').datetimepicker({
-        locale: 'en',
-        inline: true,
-        sideBySide: true,
-        format: 'L'
-      });
+    var fruitvegbasket = [];
+
+    var $calendar = $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
+      },
+
+      defaultView: 'month',
+
+      dayRender: function (date, cell) {
+        console.log("MY DATE:", date);
+        this.calendarDate = new Date(date);
+        var datestring = this.calendarDate.getFullYear() + "-" + (this.calendarDate.getMonth() + 1) + "-" + this.calendarDate.getDate()
+        var arb = writeIslamicDate(this.calendarDate);
+        fruitvegbasket.push({
+          title: 'All Day Event'+"  "+arb,
+          start: datestring,
+          allDay: true
+        });
+        console.log("MY EVE:", fruitvegbasket);
+        var today = new Date();
+        if (this.calendarDate.getDate() === today.getDate()) {
+          cell.css("background-color", "red");
+        }
+      },
+      events: fruitvegbasket,
+      viewRender: function (view) {
+
+        $('#calendar').fullCalendar('removeEvents');
+        $('#calendar').fullCalendar('addEventSource', fruitvegbasket);
+        fruitvegbasket = [];
+      }
     });
+
 
 
     this.eventService.getEvents().subscribe(data => {
       this.calendarOptions = {
         editable: true,
         eventLimit: false,
+
         locale: this.language,
         header: {
           left: 'prev,next today',
@@ -67,6 +202,14 @@ export class HomeComponent {
         dayClick: function (date, jsEvent, view) {
           alert("Selected Date:" + date);
         },
+        dayRender: function (date, cell) {
+          debugger;
+          console.log("DATE:", date);
+          var today = new Date();
+          // if (date.getDate() === today.getDate()) {
+          //   cell.css("background-color", "red");
+          // }
+        },
         fixedWeekCount: false,
         showNonCurrentDates: false
       };
@@ -76,17 +219,17 @@ export class HomeComponent {
     this.displayEvent = model;
   }
 
-   gmod(n, m) {
+  gmod(n, m) {
     return ((n % m) + m) % m;
   }
 
-   kuwaiticalendar(adjust) {
-    var today = new Date();
-    if (adjust) {
-      var adjustmili = 1000 * 60 * 60 * 24 * adjust;
-      var todaymili = today.getTime() + adjustmili;
-      var today = new Date(todaymili);
-    }
+  kuwaiticalendar(adjust) {
+    var today = new Date(adjust);
+    // if (adjust) {
+    //   var adjustmili = 1000 * 60 * 60 * 24 * adjust;
+    //   var todaymili = today.getTime() + adjustmili;
+    //   var today = new Date(todaymili);
+    // }
     var day = today.getDate();
     var month = today.getMonth();
     var year = today.getFullYear();
@@ -162,7 +305,7 @@ export class HomeComponent {
 
     return myRes;
   }
-   writeIslamicDate(adjustment) {
+  writeIslamicDate(adjustment) {
     var wdNames = new Array("Ahad", "Ithnin", "Thulatha", "Arbaa", "Khams", "Jumuah", "Sabt");
     var iMonthNames = new Array("Muharram", "Safar", "Rabi'ul Awwal", "Rabi'ul Akhir",
       "Jumadal Ula", "Jumadal Akhira", "Rajab", "Sha'ban",
@@ -182,9 +325,9 @@ export class HomeComponent {
         id: model.event.id,
         // start: model.event.start,
         // end: model.event.end,
-        title: model.event.title ,
+        title: model.event.title,
         allDay: model.event.allDay,
-        params:this.writeIslamicDate(0)
+        params: this.writeIslamicDate(0)
 
         // other params
       },
